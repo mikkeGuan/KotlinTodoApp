e0e136a2d12de105b59eb15c183e13a1b335ac09
Ruichao Guan <mikkeguan@gmail.com>
2024-09-25 20:32:47 +0300

    master changed to main

--- app/src/main/java/com/example/kotlintodo/MainActivity.kt
+++ app/src/main/java/com/example/kotlintodo/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KotlinTodoTheme {
-
+                //test
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
